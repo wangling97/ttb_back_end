e962f5bb4c425d972b24671c8bfb531937a23a2c
wangling97 <mfaturrahman97@gmail.com>
2023-02-23 12:15:13 +0700

    Root Path

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,14 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\API\ResponseTrait;
+
 class Home extends BaseController
 {
+    use ResponseTrait;
+
     public function index()
     {
-        return view('welcome_message');
+        $this->failUnauthorized();
     }
 }
